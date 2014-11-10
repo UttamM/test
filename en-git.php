f11fd19fd41099679c1a27d7c4cc71d32dc8a4b9
UttamM <umogilicherla@mobiquityinc.com>
2014-11-10 21:01:06 +0500

    Auto

--- en-git.php
+++ en-git.php
@@ -1,3 +1,18 @@
-<?php 
-echo exec('git pull');
-?>
+<?php
+
+// Init vars
+$LOCAL_ROOT         = "/var/www/html/Enernoc-Test";
+$LOCAL_REPO_NAME    = "test";
+$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+$REMOTE_REPO        = "git@github.com:UttamM/test.git";
+$DESIRED_BRANCH     = "master";
+
+// Delete local repo if it exists
+if (file_exists($LOCAL_REPO)) {
+    shell_exec("rm -rf {$LOCAL_REPO}");
+}
+
+// Clone fresh repo from github using desired local repo name and checkout the desired branch
+echo shell_exec("cd {$LOCAL_ROOT} && git pull");
+
+die("done " . mktime());
